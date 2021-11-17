35f4c6fe14841c56d741af989a9463df4c869636
JayH <b98310047@hotmail.com>
2021-11-17 17:49:12 +0800

    [DELETE] delete the $

--- src/app/pages/Home/components/Footer/index.tsx
+++ src/app/pages/Home/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer() {
 	const year = new Date().getFullYear();
 	return (
 		<footer>
-			<p>Where’s YouBike  © ${year} Code: Jay  /  Design: KT</p>
+			<p>Where’s YouBike  © {year} Code: Jay  /  Design: KT</p>
 			<p><img src={ptxLogo} alt="ptx logo" /> 交通部PTX服務平臺</p>
 		</footer>
 	)
